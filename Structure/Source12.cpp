07768696f6552d1234028d83c430bc32ffa33fc7
성수 박 <whiteibescu@gmail.com>
2020-06-24 17:52:41 +0900

    비트캠프 예제

--- Structure/Source12.cpp
+++ Structure/Source12.cpp
@@ -2,4 +2,15 @@
 
 using namespace std;
 
-inlintint
+inline int Add(int a, int b)
+{
+	return a + b;
+}
+
+void main()
+{
+	int result = 0, i = 0;
+	for (i = 0; i < 1000000; ++i)
+		result = Add(result, i++);
+	cout << result << endl;
+}
