dedbcddf4086f8abafa0b528e688f9e466ff92a2
성수 박 <whiteibescu@gmail.com>
2020-06-29 02:56:43 +0900

    과제

---
+++ _2020_06_26Assignment/_2020_06_26Assignment/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void q5() {
+    // 5. 길이가 6인 배열 arr 1, 2, 3, 4, 5, 6 으로 초기화한 다음
+    int arr[6] = { 1, 2, 3, 4, 5, 6 };
+    // 배열에 저장된 값의 순서를 6, 5, 4, 3, 2, 1로 변경한다.
+    int arr_length = sizeof(arr) / sizeof(int); // 배열 길이 일반화
+    // 배열의 앞과 뒤를 가리키는 포인터 변수 2개를 선언해서
+    int* start = arr;
+    int* end = arr + (arr_length - 1);
+    // 이를 활용하여 저장된 값의 순서를 바꿔야 한다.
+    while (start < end) {
+        int temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+    // 배열 출력
+    for (int i = 0; i < arr_length; i++) {
+        cout << "arr[" << i << "]: " << arr[i] << endl;
+    }
+}
---
+++ _2020_06_26Assignment/_2020_06_26Assignment/Source1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void q4() {
+    // 4. 길이가 5인 int형 배열 arr을 선언하고
+    // 1, 2, 3, 4, 5로 초기화한다.
+    int arr[5] = { 1,2,3,4,5 };
+    // 마지막 요소를 가리키는 ptr을 선언하고
+    int* ptr = arr;
+    ptr += sizeof(arr) / sizeof(int) - 1; // 배열의 길이와 상관없이 마지막 값을 가리키도록 일반화(ptr += 4)
+
+    // ptr을 감소하는 연산으로 배열의 요소를 모두 더하여 결과를
+    // 출력한다.
+    int sum = 0;
+    while (ptr >= arr) {
+        cout << "현재 포인터가 가리키는 값: " << *ptr << endl;
+        sum += *ptr;
+        *ptr--;
+        // 또는 ptr--;
+        // (*ptr)--; 는 불가
+    }
+    cout << "합: " << sum << endl;
+}
