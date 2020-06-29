d1b4baeb7f2f5062fcffa35f0b5f61778d2d4431
성수 박 <whiteibescu@gmail.com>
2020-06-29 20:53:31 +0900

    과제

---
+++ _2020_06_29Assignment/_2020_06_29Assignment/basic1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+
+using namespace std;
+
+class Random {
+private:
+	int n;
+public:
+	Random();
+	int next();
+	int nextInRange(int a, int b);
+};
+
+Random::Random(){ // 랜덤함수 생성
+	srand((unsigned)time(0)); // 매 번 다른 랜덤수를 발생시키기 위한 seed 설정
+}
+
+int Random::next() { // 랜덤 리턴
+	return rand();
+}
+
+int Random::nextInRange(int a, int b) {
+	return rand() % (b - a + 1) + a; //a~b까지 랜덤
+}
+
+int main() {
+	Random r;
+	cout << "-- 0에서 " << RAND_MAX << "까지의 랜덤 정수 10 개--" << endl;
+	for (int i = 0; i < 10; i++) {
+		int n = r.next(); // 0에서 RAND_MAX(32767) 사이의 랜덤한 정수
+		cout << n << ' ';
+	}
+	cout << endl << endl << "-- 2에서 " << "4 까지의 랜덤 정수 10 개 --" << endl;
+	for (int i = 0; i < 10; i++) {
+		int n = r.nextInRange(2, 4); // 2에서 4 사이의 랜덤한 정수
+		cout << n << ' ';
+	}
+	cout << endl;
+}
