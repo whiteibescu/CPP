395bc833945870936974904a3f9dc95e83139ab4
성수 박 <whiteibescu@gmail.com>
2020-07-02 18:05:07 +0900

    수업

--- Human/ComputerEngineer.h
+++ Human/ComputerEngineer.h
@@ -1,5 +1,18 @@
 #pragma once
-class ComputerEngineer
+#include "Human.h"
+class ComputerEngineer : public Human
 {
+protected:
+	int nComputer;
+
+public:
+	ComputerEngineer(string name="컴퓨터 엔지니어", 
+					int age=0, 
+					int nComputer=0);
+	~ComputerEngineer();
+
+public:
+	void overTime();	// 새로 추가한 함수
+	void info();		// 확장하는 함수
 };
 
