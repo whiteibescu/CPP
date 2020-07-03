fffd1c35f8ea87336bdd427501612e3edd8b518a
성수 박 <whiteibescu@gmail.com>
2020-07-03 09:17:28 +0900

    숙제

--- _2020_07_03Homework/middle1.cpp
+++ _2020_07_03Homework/middle1.cpp
@@ -1,49 +1,34 @@
-/*
-1. Random 클래스를 상속해서 짝수 정수만 랜덤하게 발생시키는 EvenRandom 클래스를 작성하고
-   next()를 재정의하여
-   10개의 짝수를 랜덤하게 출력하는 프로그램을 완성하세요.
-   0도 짝수로 처리합니다.
-*/
-
-
 #include <iostream>
-#include <cstdlib>
-#include <ctime> 
-
+#include <cstdlib> //atoi,rand,malloc 등의 메소드가 포함된 라이브러리
+#include <ctime> //시간과 관련된 메소드, 상수가 포함된 라이브러리
 using namespace std;
 
-// 임의의 정수를 랜덤하게 제공하는 클래스
-class Random {
+class EvenRandom {
 public:
-	Random(); // 생성자. 랜덤 seed를 설정한다.
-	int next(); // 랜던 정수를 리턴한다.
-	int nextInRange(int low, int high); // low와 high 사이의 랜덤 정수를 리턴한다.
+    EvenRandom() {
+        srand((unsigned)time(0)); //다른 랜덤수를 발생
+    }
+    int next() { //rand()함수는 정수 리턴
+        return (rand() / 2) * 2; //어떤 정수든 (홀수든 짝수든) 2를 곱하면 짝수가 된다.
+    }
+    int nextInRange(int a, int b) {
+        return (((rand() % (b - a + 1)) + a) / 2) * 2; //a이상 b미만 랜덤 정수를 뽑는 알고리즘
+
+    }
 };
 
-Random::Random() {
-	srand((unsigned)time(0)); // 임의의 seed를 설정하여 할 때마다 다른 랜덤 수가 나오게 한다.
-}
-
-int Random::next() {
-	return rand(); // 0에서 RAND_MAX 사이의 랜덤한 정수 리턴
-}
-
-int Random::nextInRange(int low, int high) {
-    int range=(high-low)+1;
-    return low + (rand() % range); // low와 high 사이의 랜덤 정수를 리턴한다. 
-}
-
-int main() {
-	Random r;
-	cout << "-- 0에서 " << RAND_MAX << "까지의 랜덤 정수 10 개--" << endl;
-	for(int i=0; i<10; i++) {
-		int n = r.next(); // 0에서 RAND_MAX(32767) 사이의 랜덤한 정수
-		cout << n << ' ';
-	}
-	cout << endl << endl << "-- 2에서 " << "4 까지의 랜덤 정수 10 개 --" << endl;
-	for(int i=0; i<10; i++) {
-		int n = r.nextInRange(2, 4); // 2에서 4 사이의 랜덤한 정수
-		cout << n << ' ';
-	}
-	cout << endl;
+int main(void) {
+    EvenRandom r; //EvenRandom 클래스의 객체 r
+    int i;
+    cout << "-- 0에서 " << RAND_MAX << "까지의 랜덤 정수 10개--" << endl; //RAND_MAX=32767 상수
+    for (i = 0; i < 10; i++) {
+        cout << r.next() << " ";  //10개 랜덤 정수 출력
+    }
+    cout << "\n\n";
+    cout << "-- 2에서 " << "10까지의 랜덤 정수 10개--" << endl;
+    for (i = 0; i < 10; i++) {
+        cout << r.nextInRange(2, 10) << " ";  //범위 내 10개 랜덤 정수 출력
+    }
+    cout << endl;
+    return 0;
 }
