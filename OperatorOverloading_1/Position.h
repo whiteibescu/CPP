4677d61a1f38dd4e5252e93c03dc88e67060ee50
성수 박 <whiteibescu@gmail.com>
2020-07-07 17:50:20 +0900

    수업

---
+++ OperatorOverloading_1/Position.h
@@ -0,0 +1,13 @@
+#pragma once
+class Position
+{
+private:
+	int x, y;
+public:
+	Position(int x = 0, int y = 0);
+public:
+	void showPosition();
+	Position& operator++();	    // 전위증가연산자
+	Position operator++(int);	// 후위증가연산자
+};
+
